85b4d672624865a2f1dc930c4a98865d0f991cd2
Sravan Kumar <mammula.sravan42384@gmail.com>
2023-01-06 11:28:50 +0530

    get ready for the conflict!!!

--- src/test/java/group1/hosp1.java
+++ src/test/java/group1/hosp1.java
@@ -3,7 +3,7 @@ package group1;
 public class hosp1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to the new hospital world");
 
 	}
 
